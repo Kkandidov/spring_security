2d2561ceff37eef889c331b40a2e363753c9c1bc
Konstantin <kkandidovvv@gmail.com>
2020-09-06 17:03:40 +0300

    security access in memory

--- src/main/java/org/astashonok/config/AppInit.java
+++ src/main/java/org/astashonok/config/AppInit.java
@@ -6,7 +6,7 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{SpringConfig.class};
+        return new Class[]{SpringConfig.class, SecurityConfig.class};
     }
 
     @Override
